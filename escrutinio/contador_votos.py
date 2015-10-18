67b8906ed6e8b25a25bb2d242c25545ae65da34d
luciaetchecopar <letchecopar@correo.um.edu.uy>
2015-10-17 21:58:44 -0300

    metodo contar

--- escrutinio/contador_votos.py
+++ escrutinio/contador_votos.py
@@ -2,10 +2,21 @@ from conexion import conexion
 
 def contar():
     cur, conn = conexion.conectar()
-    result1 = cur.execute("SELECT Host,User FROM user")
-    print(cur.description)
-    print()
-    for row in cur:
-        print(row)
+    result1 = cur.execute("select count(*) from votos where opcion_voto = 1")
+    result2 = cur.execute("select count(*) from votos where opcion_voto = 2")
+    result3 = cur.execute("select count(*) from votos where opcion_voto = 3")
+    results = {'candidato1' : result1, 'candidato2' : result2, 'en_blanco' : result3}
+    return results
+    cerrar_conexion(cur,conn)
 
-        
+#esto no se si es muy util, pero hay que poner los observados en algun lado
+def contar_observados():
+    cur, conn = conexion.conectar()
+    result1 = cur.execute("select count(*) from votos where opcion_voto = 1 and observado = true")
+    result2 = cur.execute("select count(*) from votos where opcion_voto = 2 observado = true")
+    result3 = cur.execute("select count(*) from votos where opcion_voto = 3 observado = true")
+    results = {'observados_candidato1' : result1, 'observados_candidato2' : result2, 'observados_en_blanco' : result3}
+    return results
+    cerrar_conexion(cur,conn)
+
+
