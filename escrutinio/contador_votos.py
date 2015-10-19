8bfada1232394a369767c52562a276d315998ae2
luciaetchecopar <letchecopar@correo.um.edu.uy>
2015-10-19 09:42:22 -0300

    metodo contar con run_query

--- escrutinio/contador_votos.py
+++ escrutinio/contador_votos.py
@@ -1,10 +1,15 @@
 from conexion import conexion
+from login.login import run_query
+
 
 def contar():
     cur, conn = conexion.conectar()
-    result1 = cur.execute("select count(*) from votos where opcion_voto = 1")
-    result2 = cur.execute("select count(*) from votos where opcion_voto = 2")
-    result3 = cur.execute("select count(*) from votos where opcion_voto = 3")
+    query1="select count(*) from votos where opcion_voto = 1"
+    result1 = run_query(query1)
+    query2="select count(*) from votos where opcion_voto = 2"
+    result2 = cur.run_query(query2)
+    query3="select count(*) from votos where opcion_voto = 3"
+    result3 = cur.run_query(query3)
     results = {'candidato1' : result1, 'candidato2' : result2, 'en_blanco' : result3}
     return results
     cerrar_conexion(cur,conn)
@@ -12,10 +17,13 @@ def contar():
 #esto no se si es muy util, pero hay que poner los observados en algun lado
 def contar_observados():
     cur, conn = conexion.conectar()
-    result1 = cur.execute("select count(*) from votos where opcion_voto = 1 and observado = true")
-    result2 = cur.execute("select count(*) from votos where opcion_voto = 2 observado = true")
-    result3 = cur.execute("select count(*) from votos where opcion_voto = 3 observado = true")
-    results = {'observados_candidato1' : result1, 'observados_candidato2' : result2, 'observados_en_blanco' : result3}
+    query1="select count(*) from votos where opcion_voto = 1 and observado = true"
+    result1 = run_query(query1)
+    query2="select count(*) from votos where opcion_voto = 2 observado = true"
+    result2 = cur.run_query(query2)
+    query3="select count(*) from votos where opcion_voto = 3 observado = true"
+    result3 = cur.run_query(query3)
+    results = {'candidato1' : result1, 'candidato2' : result2, 'en_blanco' : result3}
     return results
     cerrar_conexion(cur,conn)
 
