a37acf924e49f9244104cece19a7118fa95cd629
mcresci <mcresci@correo.um.edu.uy>
2015-10-19 22:41:46 -0300

    Cambios en contador_votos

--- escrutinio/contador_votos.py
+++ escrutinio/contador_votos.py
@@ -1,15 +1,15 @@
 from conexion import conexion
-from login.login import run_query
+from login import login
 
 
 def contar():
     cur, conn = conexion.conectar()
-    query1="select count(*) from votos where opcion_voto = 1"
-    result1 = run_query(query1)
-    query2="select count(*) from votos where opcion_voto = 2"
-    result2 = cur.run_query(query2)
-    query3="select count(*) from votos where opcion_voto = 3"
-    result3 = cur.run_query(query3)
+    query1= "SELECT COUNT(*) FROM `votos` WHERE `opcion_voto` = 1"
+    result1 = login.run_query(query1)
+    query2= "SELECT COUNT(*) FROM `votos` WHERE `opcion_voto` = 2"
+    result2 = login.run_query(query2)
+    query3= "SELECT COUNT(*) FROM `votos` WHERE `opcion_voto` = 3"
+    result3 = login.run_query(query3)
     results = {'candidato1' : result1, 'candidato2' : result2, 'en_blanco' : result3}
     return results
     cerrar_conexion(cur,conn)
@@ -17,12 +17,12 @@ def contar():
 #esto no se si es muy util, pero hay que poner los observados en algun lado
 def contar_observados():
     cur, conn = conexion.conectar()
-    query1="select count(*) from votos where opcion_voto = 1 and observado = true"
-    result1 = run_query(query1)
-    query2="select count(*) from votos where opcion_voto = 2 observado = true"
-    result2 = cur.run_query(query2)
-    query3="select count(*) from votos where opcion_voto = 3 observado = true"
-    result3 = cur.run_query(query3)
+    query1="SELECT COUNT(*) FROM `votos` WHERE `opcion_voto` = 1 AND `observado` = TRUE"
+    result1 = login.run_query(query1)
+    query2="SELECT COUNT(*) FROM `votos` WHERE `opcion_voto` = 2 AND `observado` = TRUE"
+    result2 = login.run_query(query2)
+    query3="SELECT COUNT(*) FROM `votos` WHERE `opcion_voto` = 3 AND `observado` = TRUE"
+    result3 = login.run_query(query3)
     results = {'candidato1' : result1, 'candidato2' : result2, 'en_blanco' : result3}
     return results
     cerrar_conexion(cur,conn)
--- login/login.py
+++ login/login.py
@@ -1,23 +1,23 @@
 from conexion import conexion
 import hashlib
+from interfaz import ventana_principal
 from conexion import conexion
 
 def login(usr,psw):
-
     query = "SELECT `contrasena` FROM `usuarios_mesa` WHERE `cedula` = " + usr
     psw_db = run_query(query)
     psw_hash1 = hashlib.sha256(psw).hexdigest()
     print psw_hash1
     print psw_db
 
-    if (psw_hash1 == psw_db):
+    if psw_hash1 == psw_db:
         #aca tenemos que llamar a ventana_principal.py
         #esto es solo una prueba
-        print(usr,psw)
+        print usr, psw
 
     else:
          print "Usuario y/o contrasena no valida"
-
+         #llamar a ventana de login
 
 
 def run_query(query=''):
