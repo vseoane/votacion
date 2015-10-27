ac1bbbd8001ecfa22819076ff4163e6a2c2f3c3c
Victoria Seoane <vseoane@dataxu.com>
2015-10-27 11:10:11 -0300

    Empezar con funcionamiento del login

--- interfaz_votacion/app.py
+++ interfaz_votacion/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, json, request
+from flask import Flask, render_template, json, request, redirect, url_for, flash
 from login import login
 app = Flask(__name__)
 
@@ -8,19 +8,20 @@ def main():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    print 'login'
     if request.method == 'GET':
         return render_template('index.html', next=request.args.get('next'))
     elif request.method == 'POST':
         username = request.form['inputName']
+        print username
         password = request.form['inputPassword']
+        print password
         valid = login.login(username, password)
         if valid:
             return redirect(url_for('main'))
         else:
             flash('Invalid login')
             return redirect(url_for('index'))
-    else:
-        return abort(405)
 
 if __name__ == "__main__":
     app.run()
